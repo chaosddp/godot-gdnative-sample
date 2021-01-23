e9f1b9bae1580a60124c5a5272d90061953b9c65
chaosyu <chaos.you@gmail.com>
2021-01-23 19:22:23 +0800

    script to build lua

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 cd godot-cpp
 
-scons platform="windows" generate_bindings=yes bits=64 target=release -j4
+# build godot-cpp
+scons platform=windows generate_bindings=yes bits=64 target=release -j4
+
+# build lua
+scons -f .\lua.scons platform=windows target=release
 
 cd ..
 
-scons platform="windows" bits=64 target=release
+scons platform=windows bits=64 target=release
+
+cp libs/win64/libnativesample.dll game/scripts/win64
